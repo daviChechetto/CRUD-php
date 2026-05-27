a0a2762d2caf2e559ed9993ff0f47a98c59d096e
davichechetto <davichechetto@gmail.com>
2026-05-27 15:33:27 -0300

    refactore - arquivo global js

--- frontend/js/global.js
+++ frontend/js/global.js
@@ -1,28 +1,33 @@
+(function () {
+
+	const UI = {
+		body: $("body"),
+	}
+
+	UI.body.append(`<div id="perseguidor"><div class="circulo"></div></div>`)
+
+
+	$(window).on('mousemove', (e) => {
+		const mouseX = e.clientX;
+		const mouseY = e.clientY;
+
+		$("#perseguidor").css({
+			'transform': `translate(${mouseX - 9}px, ${mouseY - 9}px)`
+		});
+	})
+
+	$("a, button").hover(function () {
+		$("#perseguidor").toggleClass('esconder')
+	})
+})();
+
 // A URL base da nossa API no backend
 
 const CONFIG = {
 	apiUrl: 'http://localhost/crud/backend/users'
 }
 
-$('body').append(`<div id="perseguidor"><div class="circulo"></div></div>`)
-
 function obterParametroConsulta(parametro) {
 	const parametrosUrl = new URLSearchParams(window.location.search);
 	return parametrosUrl.get(parametro);
 }
-
-$(window).on('mousemove', (e) => {
-	const mouseX = e.clientX;
-	const mouseY = e.clientY;
-
-	$("#perseguidor").css({
-		'transform': `translate(${mouseX - 9}px, ${mouseY - 9}px)`
-	});
-})
-
-$("a, button").hover( function(){
-	$("#perseguidor").toggleClass('esconder')
-})
-
-
-// O código jQuery vai começar aqui!

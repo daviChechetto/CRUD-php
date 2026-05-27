23c7c97c23bfd2632afe93f527d573dfb3ab4163
davichechetto <davichechetto@gmail.com>
2026-05-27 15:29:51 -0300

    feat - melhoria na tela login

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,21 +1,49 @@
 const UI = {
-    botaoEntrar: $("#botaoEntrar"),
-    
+	body: $("body"),
+	botaoEntrar: $("#botaoEntrar"),
+	loginForm: $("#loginForm"),
+
+	logo: $('.logo')
 }
+
+let contador = 0
 
 function piscar() {
+	UI.body.fadeOut(500).fadeIn(100);
+	$('html').append("<div class='elemento-piscar'></div>");
+}
+
+function userLogin(username, password) {
 
 }
 
-UI.botaoEntrar.on('click', function () {
-    const username = $("#username").val();
-    const password = $("#password").val();
+UI.loginForm.on('submit', function (e) {
+	e.preventDefault()
+	const username = $("#username").val();
+	const password = $("#password").val();
 
-    userLogin(username, password);
+	userLogin(username, password);
 
-    piscar();
+	piscar();
 
-    setTimeout(function () {
-        window.location.href = "index.html";
-    }, 1000);
-});
+	setTimeout(function () {
+		window.location.href = "index.html";
+	}, 1000);
+})
+
+
+UI.logo.on('click', function () {
+	if (contador == 1) {
+		contador = 0
+		UI.logo[0].animate([
+			{ transform: 'rotate(0deg)' },
+			{ transform: 'rotate(360deg)' }
+		], {
+			duration: 500,         // tempo em ms
+			iterations: 1,         // quantas vezes repete
+			easing: 'ease-in-out'  // curva de aceleração
+		});
+	} else {
+		contador += 1
+	}
+})
